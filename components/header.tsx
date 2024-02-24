a0e94784803ace18a197d749037f0b24339cbf1a
hung <dxhh2510@gmail.com>
2024-02-24 15:03:13 +0700

    color pallete

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,24 @@
-"use server";
+"use client";
 
-export default async function Header() {
-  return <>Header</>;
+import { useState } from "react";
+
+interface SocialProps {
+  icon: any;
+  label: string;
+  value: string;
+}
+
+export default function Header() {
+  const [social, setSocial] = useState<SocialProps>();
+
+  return (
+    <header className="w-full">
+      <div className="bg-tattoo-black-2 w-full" data-name="social-contact">
+        <div className="container flex justify-between"></div>
+      </div>
+      <div className="bg-tattoo-black-1 w-full" data-name="header-nav">
+        <div className="container"></div>
+      </div>
+    </header>
+  );
 }
