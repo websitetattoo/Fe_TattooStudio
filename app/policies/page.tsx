99b445a6eeaf635e52c39b01865ceccc1f9fa997
dungngoo <ngodung06vn@gmail.com>
2024-02-27 23:20:07 +0700

    Create page Policy

--- app/policies/page.tsx
+++ app/policies/page.tsx
@@ -1,5 +1,83 @@
 "use client";
+import TextTattoo from "@/app/tattoo-care/_components/text-tattoo";
+import HighlightText from "@/components/highlight-text";
+import PageTitle from "@/components/page-title";
 
 export default function index() {
-  return <>policies</>;
+  const artPolicyTitle = "At Florida Kings Tattoo, We try to keep our polices simple so the focus can remain on the art. However, we do need  to have policies in place to ensure fair treatment for all customers.";
+  const artPoliciesList = [
+    {
+      id: 1,
+      title: "Age Restriction",
+      description: [
+        "Florida Kings Tattoo often have a policy of not tattooing anyone under a certain age, typically 18 or 16 with parental consent. This is to comply with legal requirements and ensure responsible decision-making."
+      ],
+      className: true
+    },
+    {
+      id: 2,
+      title: "Health and Safety",
+      description: [
+        "Florida Kings Tattoo have strict policies to ensure the health and safety of both artists and clients. This include maintaining a clean and sterile environment, following proper sterilization and infection control procedures, using disposable and single-use equipment, and adhering to local health department regulations."
+      ],
+      className: true
+    },
+    {
+      id: 3,
+      title: "Consent and Identification",
+      description: [
+        "Florida Kings tattoo require clients to provide valid identification to prove your age and consent. This is to ensure compliance with legal requirements and protect both the client and the artist."],
+      className: true
+    },
+
+    {
+      id: 4,
+      title: "Billing Practices",
+      subtitle: "Minimum cost differs per artist. Rates will vary depending on the artist, size, design, and etc. ",
+      description: [
+        "In the consultation process, you will receive a quote. This is subject to change based on the final design and size chosen.",
+        "A non-refundable deposit is required to secure your appointment, and is taken out of the total cost of your tattoo.",
+        "Remaining balance is to be paid at the end of the session in the studio.",
+        "Major changes to the design will change the price quoted in the beginning of the inquiry."
+      ],
+    },
+    {
+      id: 5,
+      isImportant: true,
+      title: "Rescheduling Policy",
+      description: [
+        "All reschedules require a minimum of 72 hours notice in order for the deposit to be applied to a future appointment.",
+        "Any reschedules/cancellations within 72 hours before the appointment time will forfeit the total deposit.",
+        "A new non-refundable deposit will be required if you wish to book another appointment.",
+        "If an appointment is rescheduled more than two times or deemed as no-show, your deposit will be forfeited.",
+        "By submitting a deposit, you are hereby agreeing to this policy and will be held to this agreement such an event were to occur."
+      ],
+    },
+  ];
+
+  return (
+    <div className="bg-tattoo-color-bg">
+      <div className="w-full lg:w-9/12 lg:m-auto py-12">
+
+        <div className="pb-16 md:pb-8 lg:pb-20">
+          <PageTitle>
+            <HighlightText className="font-bold">
+              POLICIES
+            </HighlightText>
+          </PageTitle>
+        </div>
+        <div className="px-12 md:px-0">
+          <div className="pb-16 md:flex md:justify-center md:items-center">
+            <div className="md:pb-8 md:w-9/12 text-center text-white text-xl lg:text-2xl lg:w-full">{artPolicyTitle}</div>
+          </div>
+
+          <div className="text-left md:w-9/12 md:m-auto md:py-12 lg:w-full">
+            {artPoliciesList?.map((item, index) => (
+              <TextTattoo key={index} obj={item} className={item.className} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- app/tattoo-care/_components/text-tattoo.tsx
+++ app/tattoo-care/_components/text-tattoo.tsx
@@ -1,19 +1,28 @@
 type textTattooProps = {
   obj: any;
+  className: any;
 };
 
-export default function TextTattoo({ obj }: textTattooProps) {
+export default function TextTattoo({ obj, className }: textTattooProps) {
   return (
     <>
       <div className="w-full pb-14">
-        <h2 className="mb-2 text-center text-2xl font-bold text-white md:text-left md:text-3xl lg:text-left lg:text-4xl ">
+        <h2 className=" mb-2 text-left text-2xl font-bold text-white md:text-left md:text-3xl lg:text-left lg:text-4xl ">
+          {obj.isImportant ?
+            <span className="flex pr-4 mb-2 text-center text-2xl font-bold text-tattoo-highlight md:inline-flex md:text-left md:text-3xl lg:text-left lg:text-4xl"
+            >IMPORTANT!
+            </span>
+            : ''}
           {obj.title}
         </h2>
+        <h3 className="text-left text-xl text-tattoo-gray md:text-left md:text-2xl lg:text-left lg:text-2xl ">
+          {obj.subtitle}
+        </h3>
         {Array.isArray(obj.description) && obj.description.length > 0 && (
-          <ul className="list-disc pl-8">
+          <ul className={className ? "text-2xl h-auto" : "list-disc pl-8"}>
             {obj.description.map((line: string, index: number) => (
               <li
-                className="text-left text-base text-tattoo-gray md:text-xl lg:text-xl"
+                className="text-left text-base text-tattoo-gray md:text-xl lg:text-xl mb-1"
                 key={index}
               >
                 {line}
