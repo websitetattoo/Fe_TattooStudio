bfe68174827a1319e226f3279e6a86983e1c6684
dungngoo <ngodung06vn@gmail.com>
2024-02-28 12:31:17 +0700

    Reverting tatoo-care

--- app/tattoo-care/_components/text-tattoo.tsx
+++ app/tattoo-care/_components/text-tattoo.tsx
@@ -1,28 +1,19 @@
 type textTattooProps = {
   obj: any;
-  className: any;
 };
 
-export default function TextTattoo({ obj, className }: textTattooProps) {
+export default function TextTattoo({ obj }: textTattooProps) {
   return (
     <>
       <div className="w-full pb-14">
-        <h2 className=" mb-2 text-left text-2xl font-bold text-white md:text-left md:text-3xl lg:text-left lg:text-4xl ">
-          {obj.isImportant ?
-            <span className="flex pr-4 mb-2 text-center text-2xl font-bold text-tattoo-highlight md:inline-flex md:text-left md:text-3xl lg:text-left lg:text-4xl"
-            >IMPORTANT!
-            </span>
-            : ''}
+        <h2 className="mb-2 text-center text-2xl font-bold text-white md:text-left md:text-3xl lg:text-left lg:text-4xl ">
           {obj.title}
         </h2>
-        <h3 className="text-left text-xl text-tattoo-gray md:text-left md:text-2xl lg:text-left lg:text-2xl ">
-          {obj.subtitle}
-        </h3>
         {Array.isArray(obj.description) && obj.description.length > 0 && (
-          <ul className={className ? "text-2xl h-auto" : "list-disc pl-8"}>
+          <ul className="list-disc pl-8">
             {obj.description.map((line: string, index: number) => (
               <li
-                className="text-left text-base text-tattoo-gray md:text-xl lg:text-xl mb-1"
+                className="text-left text-base text-tattoo-gray md:text-xl lg:text-xl"
                 key={index}
               >
                 {line}
