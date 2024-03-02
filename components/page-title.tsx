3048075b1909058af6198328ff10137061b069ed
hung <dxhh2510@gmail.com>
2024-03-02 12:20:16 +0700

    studio, but gg map still using

--- components/page-title.tsx
+++ components/page-title.tsx
@@ -1,10 +1,22 @@
 import { ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 
-export default function PageTitle({ children }: { children: ReactNode }) {
+export default function PageTitle({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
   return (
     <div className="relative">
       <div className="bg-dots absolute inset-0 z-0"></div>
-      <div className="relative z-10 flex items-center justify-center py-10">
+      <div
+        className={twMerge(
+          "relative z-10 flex items-center justify-center py-10",
+          className,
+        )}
+      >
         {children}
       </div>
     </div>
