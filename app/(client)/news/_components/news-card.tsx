8926fc74739a5c9427d0fbe2fd70e9fa815ce19a
dungngoo <ngodung06vn@gmail.com>
2024-04-26 10:00:37 +0700

    "Add Efect Hover for News Card, change img to loading lazy"

--- app/(client)/news/_components/news-card.tsx
+++ app/(client)/news/_components/news-card.tsx
@@ -21,14 +21,17 @@ export default function NewsCard({
     <>
       <div>
         <CardContent className=" p-2">
-          <div className="relative m-auto h-full md:w-full">
-            <Image
-              src={imgObj.image}
-              width={700}
-              height={700}
-              alt="News Tattoo Image"
-              className="m-auto h-96 w-11/12 md:h-[300px] md:object-cover lg:w-4/5"
-            />
+          <div className="relative m-auto h-full w-full">
+            <picture className="block overflow-hidden">
+              <img
+                src={imgObj.image}
+                loading="lazy"
+                width={700}
+                height={700}
+                alt="News Tattoo Image"
+                className="m-auto h-96 duration-150 ease-in hover:scale-125 md:object-cover"
+              />
+            </picture>
             <CardTitle className="absolute  -right-0 flex w-full items-center justify-between bg-tattoo-black-1 object-cover px-10 py-6 align-middle font-sans text-lg text-yellow-50 md:-bottom-12 md:block md:w-5/12 lg:-bottom-16 lg:w-6/12">
               <span className="line-clamp-2 w-auto md:h-[56px]">
                 {imgObj.title}
